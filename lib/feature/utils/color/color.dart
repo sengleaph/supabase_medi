7ba9857376191ae5a3a752eb7c9202c6565b1189
Seng-Leaph <sengleaphseang@gmail.com>
2024-11-22 16:39:37 +0700

    mmm

--- lib/feature/utils/color/color.dart
+++ lib/feature/utils/color/color.dart
@@ -3,5 +3,4 @@ import 'dart:ui';
 var BluePrimary = const Color(0xFF133E87);
 var BlueSecondary = const Color(0xFF0D92F4);
 var WhitePrimary = const Color(0xFFE7E7E7);
-var GreyColor = Color(0xFF9F9F9F);
 var BlackColor = Color(0xFF3C3D37);
